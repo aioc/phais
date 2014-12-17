3981f2d44f48e203fe76086d560afdfd4dcad0b8
Ben Stott <bgbnbigben@gmail.com>
2014-12-18 01:31:19 +1100

    Whoops, shouldn't have put this here. Delete makefile.

--- src/core/visualisation/VisualGameEvent.java
+++ src/core/visualisation/VisualGameEvent.java
@@ -1,21 +1,7 @@
 package core.visualisation;
 
-import games.ttd.Track;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class VisualGameEvent {
 
 	int totalFrames;
 	int curFrame;
-	public List<Track> tracks;
-	
-	public VisualGameEvent() {
-		tracks = new ArrayList<Track>();
-	}
-	
-	public void addTrack(Track track) {
-		tracks.add(track);
-	}
 }
